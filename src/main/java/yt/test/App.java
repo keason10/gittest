14fb9e1a531285910f5efd9290f58011f7c29101
keason10 <729662970@.qq.com>
2018-10-27 18:38:49 +0800

    commit1

--- src/main/java/yt/test/App.java
+++ src/main/java/yt/test/App.java
@@ -6,6 +6,6 @@ package yt.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(2);
+        System.out.println("1");
     }
 }
