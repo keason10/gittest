4243ff16874e8713c1ccba1d551de98c12e3789d
keason10 <729662970@.qq.com>
2018-10-27 10:30:34 +0800

    'hello'

---
+++ src/main/java/yt/test/App.java
@@ -0,0 +1,14 @@
+package yt.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(1 + 1);
+    }
+}
