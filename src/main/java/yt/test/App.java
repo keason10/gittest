a8477c3b493e0d65ad25d169f598703a4feb2535
keason10 <729662970@qq.com>
2018-10-27 18:18:40 +0800

    Update App.java

--- src/main/java/yt/test/App.java
+++ src/main/java/yt/test/App.java
@@ -6,6 +6,6 @@ package yt.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(1 + 11);
+        System.out.println(1 + 12);
     }
 }
