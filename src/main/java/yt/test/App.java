1dafece377f92bfff50691f051db7b8a212f642b
keason10 <729662970@.qq.com>
2018-10-27 10:42:50 +0800

    'hello'

--- src/main/java/yt/test/App.java
+++ src/main/java/yt/test/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(1 + 1);
+        System.out.println(1 + 3);
     }
 }
