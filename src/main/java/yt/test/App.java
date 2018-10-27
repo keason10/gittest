da23f22c59f274fb938f572df75166474341a180
keason10 <729662970@.qq.com>
2018-10-27 10:59:14 +0800

    修改问题6

--- src/main/java/yt/test/App.java
+++ src/main/java/yt/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println("Hello World!");
-        System.out.println(1 + 5);
+        System.out.println(1 + 6);
     }
 }
